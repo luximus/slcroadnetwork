88568d72d0d739469f8ea3d29f355d64bd6ae4aa
Maggie Liu <maglu2934@gmail.com>
2023-05-10 09:49:43 -0600

    Added line of code which gets the list of all the nodes of and started work on dijkstra's algorithm

--- Main.py
+++ Main.py
@@ -1,1 +1,31 @@
-print("Please don't hurt me diego!")
+import networkx as nx
+import matplotlib.pyplot as plt
+import random 
+
+def get_graph(): 
+    graph = nx.random_regular_graph(4, 100).to_directed()
+    edges = list(graph.edges)
+    chosen_edge = random.choice(edges)
+    graph.remove_edge(chosen_edge[0], chosen_edge[1])
+    return graph
+
+#Getting a list of nodes 
+print(list(get_graph().nodes))
+
+#Finding the min distance 
+dist = {}
+prev = {}
+Q = []
+def dijkstra(get_graph(), source):
+    for n in get_graph().nodes:
+        dist[n] = float('inf')
+        prev[n] = None
+        Q.append(n)
+    dist[source] = 0
+
+    while Q.len()!=0:
+        
+
+
+
+
